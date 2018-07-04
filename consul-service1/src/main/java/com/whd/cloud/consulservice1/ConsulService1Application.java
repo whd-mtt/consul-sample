6a7566a9a9aab1f11782951b585a3dac71078c9d
wuhaodong <wuhaodong@shulianyikang.com>
2018-07-04 18:40:27 +0800

    initial commit

---
+++ consul-service1/src/main/java/com/whd/cloud/consulservice1/ConsulService1Application.java
@@ -0,0 +1,28 @@
+package com.whd.cloud.consulservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author whd
+ * @className ConsulService1Application
+ * @date 2018/7/3 11:27
+ * @desc Describe the function of this class in one sentence
+ */
+@RestController
+@SpringBootApplication
+public class ConsulService1Application {
+
+
+    @RequestMapping("/index")
+    public String home(){
+        return "<h1>Hello Consul</h1>";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulService1Application.class, args);
+    }
+}
